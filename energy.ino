e4f903db6d4fb190cb1d6a4d16570c3828aa69e3
nospamic <32896318+nospamic@users.noreply.github.com>
2019-11-14 20:12:03 +0200

    Add files via upload

--- energy.ino
+++ energy.ino
@@ -7,11 +7,12 @@ enum sensor {POWER_A = 6, POWER_B = 7};
 enum digital {DEV_A = 9, DEV_B = 10};
 enum mode {ACTIVE, PASSIVE, OPTIONS};
 
-float voltageDivisor = 22.53;
+float voltageDivisor = 24.80;
 int tryCount = 40;
 int middlePower;
 int joysticMiddleX;
 int joysticMiddleY;
+int jDeviation = 100;
 int deviationPower = 10;
 int voltagePort = 0;
 int joyPortX = 2;
@@ -22,6 +23,9 @@ int activeDevice = DEV_A;
 int activeSensor = POWER_A;
 int mode = PASSIVE;
 int buttonRepeat = 20;
+const int ARRSIZE = 50;
+float voltArray[ARRSIZE];
+int voltArrayPosition = 0;
 
 void activeMode();
 void passiveMode();
@@ -35,8 +39,9 @@ bool isButton();
 int getX();
 int getY();
 bool isPowerOn(int port);
-void tuning(const char* text, float &parametr, float minimum, float maximum);
+void tuning(const char* text, float &parametr, float minimum, float maximum, float tStep);
 bool isSave();
+void debug();
 
 
 void setup() {
@@ -48,6 +53,8 @@ void setup() {
   pinMode(12, INPUT_PULLUP);
   pinMode(DEV_A, OUTPUT);
   pinMode(DEV_B, OUTPUT);
+
+  for(int i=0; i<ARRSIZE; ++i) voltArray[i] = analogRead(voltagePort)/voltageDivisor;
 
   deviceOn(DEV_A, false);
   deviceOn(DEV_B, false);
@@ -73,12 +80,19 @@ void loop() {
 
   for (int i = 0; i < buttonRepeat; ++i) {
     if (isButton())mode = OPTIONS;
-    delay(50);
+    delay(20);
   }
 }
 
 float getVoltage(int port) {
-  return analogRead(port) / voltageDivisor;
+  float result;
+  voltArray[voltArrayPosition] = analogRead(port)/ voltageDivisor;
+  voltArrayPosition++;
+  if(voltArrayPosition == ARRSIZE)voltArrayPosition = 0;
+  for(int i=0; i<ARRSIZE; ++i){
+    result += voltArray[i];
+  }
+  return result/ARRSIZE;
 }
 
 bool isButton() {
@@ -92,7 +106,6 @@ bool isButton() {
 }
 
 int getY() {
-  int jDeviation = 100;
   int result;
   int y = analogRead(joyPortY);
   if (y < joysticMiddleY - jDeviation) result = UP;
@@ -103,8 +116,7 @@ int getY() {
 
 int getX() {
   int result;
-  int jDeviation = 100;
-  int x = analogRead(2);
+  int x = analogRead(joyPortX);
   if (x < joysticMiddleY - jDeviation) result = RIGHT;
   if (x > joysticMiddleY + jDeviation) result = LEFT;
   if (x >= joysticMiddleY - jDeviation && x <= joysticMiddleY + jDeviation) result = MIDDLE;
@@ -117,25 +129,65 @@ bool isPowerOn(int port) {
     int value = analogRead(port);
     if (value < (middlePower - deviationPower) || value > (middlePower + deviationPower))result = true;
   }
+  //lcd.clear();result?lcd.print("isPowerON"):lcd.print("isPowerOFF");delay(1000);
   return result;
 }
 
+
+void voltageTest() {
+  float voltage = getVoltage(voltagePort);
+  if (voltage >= upperVoltage) {
+    deviceOn(activeDevice, true);
+    mode = ACTIVE;
+    //lcd.clear();activeDevice == DEV_A?lcd.print("DEV_A - on"):lcd.print("DEV_B - on");
+    delay(300);
+  }
+  if (voltage <= lowerVoltage) {
+    mode = PASSIVE;
+    deviceOn(DEV_A, false);
+    deviceOn(DEV_B, false);
+    activeDevice = DEV_A;
+    activeSensor = POWER_A;
+    //lcd.clear();lcd.print("DEV_A_B - off");
+    delay(300);
+  }
+}
+
+
 void activeMode() {
-  if (!isPowerOn(activeDevice)) switching();
+  if (!isPowerOn(activeSensor)) switching();
+}
+
+void switching() {
+  
+  deviceOn(activeDevice, false);
+  if (activeDevice == DEV_A) {
+    activeDevice = DEV_B;
+    activeSensor = POWER_B;
+  }else{
+    activeDevice = DEV_A;
+    activeSensor = POWER_A;
+  }
+  //lcd.clear();
+  //lcd.print("Switch to ");activeDevice == DEV_A?lcd.print("DEV_A"):lcd.print("DEV_B");
+  //delay(1000);
 }
 
 void passiveMode() {}
 
 void optionsMode() {
+  deviceOn(activeDevice, false);
   float newUpperVoltage = upperVoltage;
   float newLowerVoltage = lowerVoltage;
   float newVoltageDivisor = voltageDivisor;
 
-  tuning("Upper_U", newUpperVoltage, 26.0, 29.0);
+  tuning("Upper_U", newUpperVoltage, 26.0, 29.0, 0.1);
   delay(400);
-  tuning("Lower_U", newLowerVoltage, 20.0, 26.0);
+  tuning("Lower_U", newLowerVoltage, 20.0, 26.0, 0.1);
   delay(400);
-  tuning("Divisor", newVoltageDivisor, 15.0, 30.0);
+  tuning("Divisor", newVoltageDivisor, 15.0, 30.0, 0.05);
+  //delay(400);
+  //debug();
   delay(200);
   if (isSave()) {
     upperVoltage = newUpperVoltage;
@@ -149,68 +201,46 @@ void deviceOn(int port, bool state) {
   digitalWrite(port, state);
 }
 
-void switching() {
-  deviceOn(activeDevice, false);
-  if (activeDevice == DEV_A) {
-    activeDevice = DEV_B;
-    activeSensor = POWER_B;
-  }
-  if (activeDevice == DEV_B) {
-    activeDevice = DEV_A;
-    activeSensor = POWER_A;
-  }
-}
 
-void voltageTest() {
-  if (getVoltage(voltagePort) >= upperVoltage) {
-    mode = ACTIVE;
-    deviceOn(activeDevice, true);
-  }
-  if (getVoltage(voltagePort) <= lowerVoltage) {
-    mode = PASSIVE;
-    deviceOn(activeDevice, false);
-    activeDevice = DEV_A;
-    activeSensor = POWER_A;
-  }
-}
 
 void showInfo() {
-  lcd.clear();
+  float voltage = getVoltage(voltagePort);
+  //lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("U=");
-  lcd.print(getVoltage(voltagePort));
+  lcd.print(voltage);
   lcd.print("v ");
-  if (mode == ACTIVE && activeDevice == DEV_A) lcd.print("Dev.A");
-  if (mode == ACTIVE && activeDevice == DEV_B) lcd.print("Dev.B");
-  if (mode == PASSIVE)lcd.print("Passive");
+  if (mode == ACTIVE && activeDevice == DEV_A) lcd.print("Dev.A  ");
+  if (mode == ACTIVE && activeDevice == DEV_B) lcd.print("Dev.B  ");
+  if (mode == PASSIVE)lcd.print("Passive ");
   lcd.setCursor(0, 1);
   lcd.print("Button->options");
+  delay(10);
 }
 
-void tuning(const char* text, float &parametr, float minimum, float maximum) {
-  float step1 = 0.1;
+void tuning(const char* text, float &parametr, float minimum, float maximum, float tStep) {
   while (!isButton()) {
     lcd.clear();
     lcd.print("Options: ");
     lcd.print(text);
     lcd.setCursor(0, 1);
-    lcd.print("<-  ");
+    lcd.print("<(-) ");
     lcd.print(parametr);
-    lcd.print(" ->");
+    lcd.print(" (+)>");
     delay(200);
     while (!isButton()) {
       if (getX() == RIGHT) {
-        parametr += step1;
+        parametr += tStep;
         break;
       }
       if (getX() == LEFT) {
-        parametr -= step1;
+        parametr -= tStep;
         break;
       }
       delay(50);
     }
-    if (parametr < minimum) parametr += step1;
-    if (parametr > maximum) parametr -= step1;
+    if (parametr < minimum) parametr += tStep;
+    if (parametr > maximum) parametr -= tStep;
   }
 }
 
@@ -236,4 +266,23 @@ bool isSave() {
   }
   delay(200);
   return answer;
+}
+
+void debug(){
+  lcd.clear();
+  deviceOn(DEV_A, true); deviceOn(DEV_B, false);
+  delay(300);
+  if(isPowerOn(POWER_A))lcd.print("POWER_A");
+  deviceOn(DEV_B, true); deviceOn(DEV_A, false);
+  delay(300);
+  if(isPowerOn(POWER_B))lcd.print("POWER_B");
+  
+  while(!isButton()){
+    lcd.setCursor(0,1);  
+    lcd.print("Vp-");
+    lcd.print(analogRead(voltagePort));
+   
+    
+    delay(400);
+  }
 }
